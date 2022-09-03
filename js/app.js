927b895c1f812801f9da4421bf504ee69b2d701d
Anny27-pixel <jabintasmin102523@gmail.com>
2022-09-03 15:19:49 +0600

    loaded specific News

--- js/app.js
+++ js/app.js
@@ -6,17 +6,42 @@ const loadAllCategory = async () => {
 }
 
 const displayAllCategory = categories => {
+    // console.log(categories);
     const categoryContainer = document.getElementById('category-container');
     for (const category of categories) {
         const categoryDiv = document.createElement('div');
-        categoryDiv.classList.add('px-4');
         categoryDiv.innerHTML = `
-        <a href="">${category.category_name}</a>
+        <div class="px-2">
+         <Button onclick = "loadSpecificNews('${category.category_id}')" class =" btn btn-light ">${category.category_name}</button>
+        </div>
         `;
+
         categoryContainer.appendChild(categoryDiv);
+        // console.log(category.category_id);
     }
+}
+
+const loadSpecificNews = async (category_id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    // console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+
+const displaySpecificNews = news = {
+
+
+
 }
 
 
 
+loadSpecificNews();
+
+
+
+
 loadAllCategory();
+
+// document.getElementById('category.category_id');
